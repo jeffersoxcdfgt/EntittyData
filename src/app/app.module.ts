c850096a19e6f29df64f0ef06fd891853fdfccc7
jefferson <jefferson.medina@sourcemeridian.com>
2021-10-09 12:58:50 -0500

    Install packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { AppInMemoryApi } from './app.in-memory.api';
+
 
 import { AppComponent } from './app.component';
 
@@ -8,6 +12,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    HttpClientModule,
+     HttpClientInMemoryWebApiModule.forRoot(AppInMemoryApi),
     BrowserModule
   ],
   providers: [],
